d7ddf9d3fb5a936236f823a8527b8b527c83c4a2
Zeel Bhanderi <zeelbhanderi28@gmail.com>
2024-03-05 22:45:41 +0530

    Data modelling - TODO

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-     res.send('MY APP')
-})
+app.get("/", (req, res) => {
+  res.send("MY APP");
+});
 
-app.get('/dashboard', function (req, res) {
-  res.send('dashboard')
-})
+app.get("/dashboard", function (req, res) {
+  res.send("dashboard");
+});
 const port = process.env.PORT || 4000;
-app.listen(port,()=>{
-     console.log(`App is runing http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`App is runing http://localhost:${port}`);
+});
